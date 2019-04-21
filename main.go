ff75d0aef34f7b78f0fe2e32310582f41a48e4ce
David Taylor <taylor.david.ray@gmail.com>
2019-04-21 12:11:09 -0700

    creates nilTracer for default operation

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+  "flag"
   "log"
   "net/http"
   "text/template"
   "path/filepath"
   "sync"
+  "os"
+  "github.com/cornjacket/trace"
 )
 
 // templ represents a signle template
@@ -20,17 +23,21 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   t.once.Do(func() {
     t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
   })
-  t.templ.Execute(w, nil)
+  t.templ.Execute(w, r)
 }
 
 func main() {
+  var addr = flag.String("addr", ":8082", "The addr of the application.")
+  flag.Parse() // parse the flags
   r := newRoom()
+  r.tracer = trace.New(os.Stdout)
   http.Handle("/", &templateHandler{filename: "chat.html"})
   http.Handle("/room", r)
   // get the room going
   go r.run()
   // start the web server
-  if err := http.ListenAndServe(":8082", nil); err != nil {
+  log.Println("Starting web server on", *addr)
+  if err := http.ListenAndServe(*addr, nil); err != nil {
     log.Fatal("ListenAndServe:", err)
   }
 }
--- room.go
+++ room.go
@@ -5,6 +5,7 @@ import (
   "net/http"
 
   "github.com/gorilla/websocket"
+  "github.com/cornjacket/trace"
 )
 
 type room struct {
@@ -17,6 +18,9 @@ type room struct {
   leave chan *client
   // clients holds all current clients in this room
   clients map[*client]bool
+  // tracer will receive trace information of activity
+  // in the room
+  tracer trace.Tracer
 }
 
 // newRoom makes a new room that is ready to go
@@ -26,6 +30,7 @@ func newRoom() *room {
     join:    make(chan *client),
     leave:   make(chan *client),
     clients: make(map[*client]bool),
+    tracer:  trace.Off(),
   }
 }
 
@@ -35,20 +40,24 @@ func (r *room) run() {
     case client := <-r.join:
       // joining
       r.clients[client] = true
+      r.tracer.Trace("New client joined")
     case client := <-r.leave:
       // leaving
       delete(r.clients, client)
       close(client.send)
+      r.tracer.Trace("Client left")
     case msg := <-r.forward:
       // forward message to all clients
       for client := range r.clients {
         select {
         case client.send <- msg:
           // send the message
+	  r.tracer.Trace(" -- send to client")
         default:
           // failed to send
           delete(r.clients, client)
           close(client.send)
+	  r.tracer.Trace(" -- failed to send, cleaned up client")
         }
       }
     }
